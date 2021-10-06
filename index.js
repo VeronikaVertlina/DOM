c08482adeeef00f8a748f271f6bdd49f9c6b259e
ronya78@gmail.com <ronya78@gmail.com>
2021-10-06 23:55:28 +0300

    Add code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("Hello");
+ alert("Hello");
+
+// document.querySelector("h1").innerHTML = "Good Bye";
+
+document.querySelector("li").nextElementSibling.
+nextElementSibling.innerHTML = "Veronika Vertlina";
+
+// document.firstElementChild.lastElementChild.firstElementChild.
+// nextElementSibling.nextElementSibling.
+// nextElementSibling.lastElementChild.innerHTML = "Angela";
